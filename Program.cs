3785c1fe71f03c16fcd7c0bb9edd7af2cc7c352e
Plamena Nickolova <47273192+Plamena02@users.noreply.github.com>
2019-04-07 08:09:20 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Цели_числа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a =sbyte.Parse(Console.ReadLine());
+            byte b = byte.Parse(Console.ReadLine()); 
+            short c = short.Parse(Console.ReadLine()); 
+            ushort d = ushort.Parse(Console.ReadLine());
+            uint e = uint.Parse(Console.ReadLine());
+            int f = int.Parse(Console.ReadLine());
+            long g = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+
+        }
+    }
+}
